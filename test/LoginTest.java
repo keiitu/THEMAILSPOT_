c34309089026243211ebcaf7beebe973d53ff0ec
keiitu <keiitum3tse@gmail.com>
2026-09-09 10:04:50 +0200

    Add: Added LoginTest class

---
+++ test/LoginTest.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.thespot.themailspot.identitymanagement.Login;
+/**
+ *
+ * @author Keitumetse Chief Mamakoko: Student @Rosebank International - ST10496139
+ * 
+ * POE: Part 01 of 03 (Registration and Login Features)
+ * 
+ * 
+ * 
+ */
+
+//This program will verify that methods are executing as expected
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+    //assertTrue/False tests
+
+    
+}
